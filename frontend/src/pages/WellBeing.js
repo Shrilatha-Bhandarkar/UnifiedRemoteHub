2ce49e840a0c4b6071bd64c1e86d2485b3d1df2c
spoorthi <spoorthiachar9@gmail.com>
2023-11-29 20:39:31 +0530

    Stopwatch and audio player

--- frontend/src/pages/WellBeing.js
+++ frontend/src/pages/WellBeing.js
@@ -5,6 +5,9 @@ export default function WellBeing() {
   const [time, setTime] = useState(0);
   const [running, setRunning] = useState(true);
   const [isStarted, setIsStarted] = useState(false);
+  const [audioElement, setAudioElement] = useState(null);
+  const [isAudioPlaying, setIsAudioPlaying] = useState(false);
+  const [selectedFile, setSelectedFile] = useState(null);
   const timer = useRef();
 
   useEffect(() => {
@@ -17,9 +20,35 @@ export default function WellBeing() {
     return () => clearInterval(timer.current);
   }, [isStarted]);
 
+  useEffect(() => {
+    if (selectedFile) {
+      const audio = new Audio(URL.createObjectURL(selectedFile));
+      setAudioElement(audio);
+    }
+  }, [selectedFile]);
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    setSelectedFile(file);
+  };
+
+  const playAudio = () => {
+    if (audioElement) {
+      audioElement.play();
+      setIsAudioPlaying(true);
+    }
+  };
+
+  useEffect(() => {
+    if (!running && isAudioPlaying) {
+      audioElement.pause();
+      setIsAudioPlaying(false);
+    }
+  }, [running, isAudioPlaying, audioElement]);
   const handleStartClick = () => {
     setIsStarted(true);
   };
+
 
   return (
     <div>
@@ -27,7 +56,7 @@ export default function WellBeing() {
       <div className='stopwatch'>
         <p className='timer'>{format(time)}</p>
         <div className='actions'>
-          <button onClick={() => setTime(0)}>Restart</button>
+        <button onClick={() => setTime(0)}>Restart</button>
           <button
             onClick={() => {
               if (isStarted) {
@@ -40,6 +69,11 @@ export default function WellBeing() {
             {running ? 'Stop' : 'Resume'}
           </button>
           <button onClick={handleStartClick}>Start</button>
+          <div>
+          <input type="file" onChange={handleFileChange} />
+          <button onClick={playAudio}>Play Audio</button>
+          </div>
+          
         </div>
       </div>
     </div>
@@ -47,11 +81,12 @@ export default function WellBeing() {
 }
 
 const format = (time) => {
-  let hours = Math.floor((time / 60 / 60) % 24);
+    let hours = Math.floor((time / 60 / 60) % 24);
   let minutes = Math.floor((time / 60) % 60);
   let seconds = Math.floor(time % 60);
   hours = hours < 10 ? '0' + hours : hours;
   minutes = minutes < 10 ? '0' + minutes : minutes;
   seconds = seconds < 10 ? '0' + seconds : seconds;
   return hours + ':' + minutes + ':' + seconds;
+ 
 };
