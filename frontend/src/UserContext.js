f6240f5d0afe1384d3bbd32e587a70da0d9c570e
BhatNishanthGanesh <nishanth.cs21@sahyadri.edu.in>
2023-11-29 22:46:00 +0530

    changed userId->null to storing in localstorage

--- frontend/src/UserContext.js
+++ frontend/src/UserContext.js
@@ -13,12 +13,24 @@
 //   );
 // };
 
-import React, { createContext, useState, useContext } from 'react';
+import React, { createContext, useState, useContext, useEffect } from 'react';
 
 const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
-  const [userId, setUserId] = useState(null);
+  const [userId, setUserId] = useState(() => {
+    // Retrieve userId from localStorage on initial load
+    return localStorage.getItem('userId') || null;
+  });
+
+  useEffect(() => {
+    // Update localStorage when userId changes
+    if (userId) {
+      localStorage.setItem('userId', userId);
+    } else {
+      localStorage.removeItem('userId');
+    }
+  }, [userId]);
 
   return (
     <UserContext.Provider value={{ userId, setUserId }}>
