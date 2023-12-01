ad3969fac510093a67d8b062b19185e540b11256
spoorthi <spoorthiachar9@gmail.com>
2023-12-01 11:13:40 +0530

    some changes

--- frontend/src/components/Work.js
+++ frontend/src/components/Work.js
@@ -6,17 +6,28 @@ import { Route, Routes, BrowserRouter as Router, Link } from 'react-router-dom';
 const Work = () => {
   return (
       <nav>
-            <Link to="/">Workspace</Link>
-          
-            <Link to="/announcement">Announcement</Link>
-         
-            <Link to="/projects">Projects</Link>
-          
-            <Link to="/ongoing">Ongoing Tasks</Link>
-         
-            <Link to="/meetings">Meetings</Link>
-         
-            <Link to="/chatting">Chat</Link>
+        <ul>
+          <li>
+          <Link to="/">Workspace</Link>
+          </li>
+          <li>
+          <Link to="/announcement">Announcement</Link>
+          </li>
+          <li>
+          <Link to="/projects">Projects</Link>
+          </li>
+          <li>
+          <Link to="/ongoing">Ongoing Tasks</Link>
+          </li>
+          <li>
+          <Link to="/meetings">Meetings</Link>
+          </li>
+          <li>
+          <Link to="/chatting">Chat</Link>
+          </li>
+       
+         </ul>
+            
           
       </nav>
   );
