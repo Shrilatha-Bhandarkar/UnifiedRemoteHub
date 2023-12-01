c87012fe8ec592f6454195b34a9d9a52d92ba20e
spoorthi <spoorthiachar9@gmail.com>
2023-12-01 10:08:56 +0530

    some changes

--- frontend/src/components/Work.js
+++ frontend/src/components/Work.js
@@ -1,16 +1,51 @@
-// Add these imports at the top of your file
-import { Route, Routes, useNavigate } from 'react-router-dom';
+// Dashboard.js
+import React from 'react';
+import { Route, Routes, BrowserRouter as Router, Link } from 'react-router-dom';
+import Workspace from './Workspace'; // Import your Workspace component
 import AnnouncementPage from '../pages/WorkspaceSection';
 import ProjectsPage from '../pages/ProjectsPage';
 import OngoingPage from '../pages/OngoingTasksPage';
 import MeetingsPage from '../pages/MeetingsPage';
 import ChattingPage from '../pages/ChatPage';
 
-// Inside your component, add these routes within the <Routes> component
-<Routes>
-  <Route path="announcement" element={<AnnouncementPage />} />
-  <Route path="projects" element={<ProjectsPage />} />
-  <Route path="ongoing" element={<OngoingPage />} />
-  <Route path="meetings" element={<MeetingsPage />} />
-  <Route path="chatting" element={<ChattingPage />} />
-</Routes>
+const Dashboard = () => {
+  return (
+    <Router>
+      {/* Navigation Links */}
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Workspace</Link>
+          </li>
+          <li>
+            <Link to="announcement">Announcement</Link>
+          </li>
+          <li>
+            <Link to="projects">Projects</Link>
+          </li>
+          <li>
+            <Link to="ongoing">Ongoing Tasks</Link>
+          </li>
+          <li>
+            <Link to="meetings">Meetings</Link>
+          </li>
+          <li>
+            <Link to="chatting">Chat</Link>
+          </li>
+        </ul>
+      </nav>
+
+      {/* Routes */}
+      <Routes>
+        <Route path="/" element={<Workspace />} />
+        <Route path="announcement" element={<AnnouncementPage />} />
+        <Route path="projects" element={<ProjectsPage />} />
+        <Route path="ongoing" element={<OngoingPage />} />
+        <Route path="meetings" element={<MeetingsPage />} />
+        <Route path="chatting" element={<ChattingPage />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default Dashboard;
