706359aa58efe37306aa5ec5dabb23ee844b9f42
dezuivere <shwetharao2003@gmail.com>
2023-12-01 11:10:16 +0530

    chatbot added

--- frontend/src/components/Work.js
+++ frontend/src/components/Work.js
@@ -16,7 +16,7 @@ const Work = () => {
          
             <Link to="/meetings">Meetings</Link>
          
-            <Link to="/chatting">Chat</Link>
+            <a href=' http://127.0.0.1:7860'>project assistant</a>
           
       </nav>
   );
